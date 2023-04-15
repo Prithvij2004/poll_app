4e2a0595014fdc02d346eca9815cb72c0da80235
Prithvij2004 <manikandan2200j@gmail.com>
2023-04-15 22:31:14 +0530

    polls_app

---
+++ mysite/urls.py
@@ -0,0 +1,10 @@
+import debug_toolbar
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('__debug__/', include(debug_toolbar.urls)),
+    path('admin/', admin.site.urls),
+    path('polls/', include("polls.urls")),
+]
